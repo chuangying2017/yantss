01b1294e4162b1491d1473dcb28c729e8e37f6c3
RLflash <2039400885@qq.com>
2018-05-23 19:43:29 +0800

    185231943

--- yt_client/src/components/Subscribe/index.vue
+++ yt_client/src/components/Subscribe/index.vue
@@ -1022,8 +1022,8 @@
   #yt-mall .m-home-grid.m-home-grid-icon .icon {
     display: block;
     margin: 0 auto;
-    width: 55px;
-    height: 62px;
+    width: 45px;
+    height: 51px;
     background: #C71A40;
     border-radius: .8rem;
     text-align: center;
