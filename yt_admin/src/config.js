185466ca19bc5e25fbb259b7aa2724d56907b6f7
RLflash <2039400885@qq.com>
2018-05-31 20:48:16 +0800

    fesfes

--- yt_admin/src/config.js
+++ yt_admin/src/config.js
@@ -1,16 +1,16 @@
-//export const API_ROOT = (process.env.NODE_ENV === 'production')
-//? 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-//: 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-//export const QINIU_URL = 'http://o7tep4eu1.bkt.clouddn.com/'
-//export const STATION_DOMAIN = (process.env.NODE_ENV === 'production')
-//? 'http://yt2.l43.cn'
-//: 'http://yt2.l43.cn'
-//  // : 'http://test.station.yt.weazm.com'
-    //  修改之前
 export const API_ROOT = (process.env.NODE_ENV === 'production')
-? 'http://192.168.10.14:805/weazm-yt-api-fca648ea00e7/server.php/api'
-: 'http://192.168.10.14:805/weazm-yt-api-fca648ea00e7/server.php/api'
+? 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+: 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
 export const QINIU_URL = 'http://o7tep4eu1.bkt.clouddn.com/'
 export const STATION_DOMAIN = (process.env.NODE_ENV === 'production')
-? 'http://192.168.10.14:805'
-: 'http://192.168.10.14:805'
+? 'http://yt2.l43.cn'
+: 'http://yt2.l43.cn'
+    // : 'http://test.station.yt.weazm.com'
+    //  修改之前
+//export const API_ROOT = (process.env.NODE_ENV === 'production')
+//? 'http://192.168.10.14:805/weazm-yt-api-fca648ea00e7/server.php/api'
+//: 'http://192.168.10.14:805/weazm-yt-api-fca648ea00e7/server.php/api'
+//export const QINIU_URL = 'http://o7tep4eu1.bkt.clouddn.com/'
+//export const STATION_DOMAIN = (process.env.NODE_ENV === 'production')
+//? 'http://192.168.10.14:805'
+//: 'http://192.168.10.14:805'
