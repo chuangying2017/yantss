5b0a0cbbc0df8f9e75852ddc8c52b16cc6dbe101
RLflash <2039400885@qq.com>
2018-06-01 03:28:28 +0800

    fesafee

--- yt_staff/src/config.js
+++ yt_staff/src/config.js
@@ -1,32 +1,32 @@
+export const API_ROOT = (process.env.NODE_ENV === 'production')
+? 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+: 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+export const HOST = (process.env.NODE_ENV === 'production')
+? 'http://yt.l43.cn/yt-client'
+: 'http://yt.l43.cn/yt-client'
+export const MIXPANEL_KEY = (process.env.NODE_ENV === 'production')
+? 'ceada88e3273a06cf4a632a2a8135656'
+: '6320bb850dacebc8138749c5c14355f2'
+export const STAFF_ACTIVITY = (process.env.NODE_ENV === 'production')
+? 'http://yt.l43.cn/yt-client/?#!/activities/activity_staff_refer_2016'
+: 'http://yt.l43.cn/yt-client/?#!/activities/activity_5812bdd6a6ac1'
+export const WILDDOG_STAFF = (process.env.NODE_ENV === 'production')
+? 'https://staff.wilddogio.com'
+: 'https://staff.wilddogio.com'
+//
 //export const API_ROOT = (process.env.NODE_ENV === 'production')
-//? 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-//: 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+//? 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+//: 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
 //export const HOST = (process.env.NODE_ENV === 'production')
-//? 'http://yt.l43.cn/yt-client'
-//: 'http://yt.l43.cn/yt-client'
+//? 'http://yt2.l43.cn/yt-client'
+//: 'http://yt2.l43.cn/yt-client'
 //export const MIXPANEL_KEY = (process.env.NODE_ENV === 'production')
 //? 'ceada88e3273a06cf4a632a2a8135656'
 //: '6320bb850dacebc8138749c5c14355f2'
 //export const STAFF_ACTIVITY = (process.env.NODE_ENV === 'production')
-//? 'http://yt.l43.cn/yt-client/?#!/activities/activity_staff_refer_2016'
-//: 'http://yt.l43.cn/yt-client/?#!/activities/activity_5812bdd6a6ac1'
+//? 'http://yt2.l43.cn/yt-client/?#!/activities/activity_staff_refer_2016'
+//: 'http://yt2.l43.cn/yt-client/?#!/activities/activity_5812bdd6a6ac1'
 //export const WILDDOG_STAFF = (process.env.NODE_ENV === 'production')
 //? 'https://staff.wilddogio.com'
 //: 'https://staff.wilddogio.com'
 //
-export const API_ROOT = (process.env.NODE_ENV === 'production')
-  ? 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-  : 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-export const HOST = (process.env.NODE_ENV === 'production')
-  ? 'http://yt2.l43.cn/yt-client'
-  : 'http://yt2.l43.cn/yt-client'
-export const MIXPANEL_KEY = (process.env.NODE_ENV === 'production')
-  ? 'ceada88e3273a06cf4a632a2a8135656'
-  : '6320bb850dacebc8138749c5c14355f2'
-export const STAFF_ACTIVITY = (process.env.NODE_ENV === 'production')
-  ? 'http://yt2.l43.cn/yt-client/?#!/activities/activity_staff_refer_2016'
-  : 'http://yt2.l43.cn/yt-client/?#!/activities/activity_5812bdd6a6ac1'
-export const WILDDOG_STAFF = (process.env.NODE_ENV === 'production')
-  ? 'https://staff.wilddogio.com'
-  : 'https://staff.wilddogio.com'
-
