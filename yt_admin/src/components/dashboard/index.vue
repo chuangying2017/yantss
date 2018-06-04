119ae035a31e486b60db71eb406ed27bf9727926
RLflash <2039400885@qq.com>
2018-06-04 18:11:43 +0800

    fesfes

--- yt_admin/src/components/dashboard/index.vue
+++ yt_admin/src/components/dashboard/index.vue
@@ -221,18 +221,18 @@
               <!--<li><a v-link="{path: '/dashboard/access/roles/list'}"><i class="fa fa-users"></i> 角色列表</a></li>-->
             </ul>
           </li>
-          <!--<li class="treeview" v-if="permit('AssessList')" :class="{active: route.nav == 'assessList'}">-->
-            <!--<a href="#">-->
-              <!--<i class="fa fa-gift"></i>-->
-              <!--<span>评价</span>-->
-              <!--<i class="fa fa-angle-left pull-right"></i>-->
-            <!--</a>-->
-            <!--<ul class="treeview-menu">-->
-              <!--<li><a v-link="{path: '/dashboard/assess/list'}"><i class="fa fa-navicon"></i> 用户评价</a></li>-->
-              <!--<li><a v-link="{path: '/dashboard/assess/starTag'}"><i class="fa fa-cubes"></i> 评价设置</a></li>-->
-              <!--<li><a v-link="{path: '/dashboard/assess/ranklist'}"><i class="fa fa-sitemap"></i> 好友率排行</a></li>-->
-            <!--</ul>-->
-          <!--</li>-->
+          <li class="treeview" v-if="permit('AssessList')" :class="{active: route.nav == 'assessList'}">
+            <a href="#">
+              <i class="fa fa-gift"></i>
+              <span>评价</span>
+              <i class="fa fa-angle-left pull-right"></i>
+           </a>
+            <ul class="treeview-menu">
+              <li><a v-link="{path: '/dashboard/assess/list'}"><i class="fa fa-navicon"></i> 用户评价</a></li>
+              <li><a v-link="{path: '/dashboard/assess/starTag'}"><i class="fa fa-cubes"></i> 评价设置</a></li>
+              <li><a v-link="{path: '/dashboard/assess/ranklist'}"><i class="fa fa-sitemap"></i> 好友率排行</a></li>
+            </ul>
+          </li>
         </ul>
       </section>
       <!-- /.sidebar -->
