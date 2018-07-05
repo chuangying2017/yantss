7710bd4c34cb62142966c08d79ff919010fa03f2
chuangying2017 <631711487@qq.com>
2018-07-05 22:05:07 +0800

    aaa

--- yt_client/src/components/Auth/wechat.vue
+++ yt_client/src/components/Auth/wechat.vue
@@ -6,25 +6,25 @@
   export default {
     route: {
       activate: function () {
-        var self = this
-        // 获取登录链接
-        self.$http.get('/auth/login/weixin?role=client').then(
-          function (data) {
-            // 获取成功，准备跳转
-          
-            var url = data.data.data.url
-        
-            window.location.href = url
-            
-          },
-          function (data) {
-            // 获取失败，回到首页
-          
-            window.alert('获取微信登录链接失败，请重试')
-            self.$route.router.go('/')
-          }
-        )
-      }
+            var self = this
+            // 获取登录链接
+            self.$http.get('/auth/login/weixin?role=client').then(
+                function (data) {
+                    // 获取成功，准备跳转
+
+                    var url = data.data.data.url
+
+                    window.location.href = url
+
+                },
+                function (data) {
+                    // 获取失败，回到首页
+
+                    window.alert('获取微信登录链接失败，请重试')
+                    self.$route.router.go('/')
+                }
+            )
+        }
     }
   }
 </script>
