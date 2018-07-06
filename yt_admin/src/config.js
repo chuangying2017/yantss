8a1f3e1c3949f780d4a371901e1abd770b01d823
chuangying2017 <631711487@qq.com>
2018-07-06 14:03:33 +0800

    genggai

--- yt_admin/src/config.js
+++ yt_admin/src/config.js
@@ -1,7 +1,7 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
 ? 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
 : 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-export const QINIU_URL = 'http://o7tep4eu1.bkt.clouddn.com/'
+export const QINIU_URL = 'http://pbfkayj1e.bkt.clouddn.com/'
 export const STATION_DOMAIN = (process.env.NODE_ENV === 'production')
 ? 'http://yt.l43.cn'
 : 'http://yt.l43.cn'
