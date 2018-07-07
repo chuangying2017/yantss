385172767676b376dfd5a6127dd0492272c0b13a
chuangying2017 <631711487@qq.com>
2018-07-07 16:07:22 +0800

    update admin manager

--- yt_admin/src/config.js
+++ yt_admin/src/config.js
@@ -1,10 +1,10 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
-? 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-: 'http://yt.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
-export const QINIU_URL = 'http://pbfkayj1e.bkt.clouddn.com/'
+? 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+: 'http://yt2.l43.cn/weazm-yt-api-fca648ea00e7/server.php/api'
+export const QINIU_URL = 'http://pbfwg0nel.bkt.clouddn.com/'
 export const STATION_DOMAIN = (process.env.NODE_ENV === 'production')
-? 'http://yt.l43.cn'
-: 'http://yt.l43.cn'
+? 'http://yt2.l43.cn'
+: 'http://yt2.l43.cn'
     // : 'http://test.station.yt.weazm.com'
     //  修改之前
 //export const API_ROOT = (process.env.NODE_ENV === 'production')
