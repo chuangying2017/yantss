975461a0c7951ceca7ac67bbecc5814029b24a1e
chuangying2017 <631711487@qq.com>
2018-07-13 09:16:26 +0800

    genggaiintegral

--- yt_admin/src/components/dashboard/integral/categories/addcategory.vue
+++ yt_admin/src/components/dashboard/integral/categories/addcategory.vue
@@ -1,11 +1,10 @@
 <template>
-  <gallery :limit="5" :images.sync="images"></gallery>
+  <gallery :limit="5" :coverlimit="limittype" :images.sync="images"></gallery>
   <div class="row" v-if="!$loadRouteData">
     <div class="col-xs-12">
       <div class="nav-tabs-custom">
         <ul class="nav nav-tabs" role="tablist">
-          <li role="presentation" class="active"><a href="#basic" data-toggle="tab" aria-controls="basic" role="tab"
-                                                    data-toggle="tab">基本信息</a></li>
+          <li role="presentation" class="active"><a href="#basic" data-toggle="tab" aria-controls="basic" role="tab" data-toggle="tab">基本信息</a></li>
         </ul>
         <div class="tab-content">
           <div role="tabpanel" class="tab-pane active" id="basic">
@@ -16,13 +15,13 @@
                     <div class="form-group">
                       <label for="inputEmail3" class="col-sm-2 control-label">排序</label>
                       <div class="col-sm-6">
-                        <input type="number" class="form-control" v-model="integral.file_sort" number>
+                        <input type="number" class="form-control" v-model="integralData.sort_type" number>
                       </div>
                     </div>
                     <div class="form-group">
                       <label for="inputEmail3" class="col-sm-2 control-label">分类名称</label>
                       <div class="col-sm-6">
-                        <input type="text" class="form-control" v-model="integral.nickname">
+                        <input type="text" class="form-control" v-model="integralData.title">
                       </div>
                     </div>
                     <div class="form-group">
@@ -33,30 +32,8 @@
                           <div class="col-xs-6 col-md-3" v-for="image in images" track-by="$index">
                             <div href="#" class="thumbnail thumbnail-mask">
                               <img :src="image.url" alt="" width="64">
-                              <div class="cover-mask" @click="setCover($index, image)">
-                                <span>设为封面</span>
-                              </div>
-                              <span class="cover-label" v-if="images[$index]['cover']">封面</span>
                             </div>
                           </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="inputEmail3" class="col-sm-2 control-label">是否首页推荐</label>
-
-                      <div class="col-sm-8">
-                        <div class="radio">
-                          <label for="">
-                            <input type="radio" number v-model="giftcard.active" value="1">
-                            是
-                          </label>
-                        </div>
-                        <div class="radio">
-                          <label for="">
-                            <input type="radio" number v-model="giftcard.active" value="0" checked="checked">
-                            否
-                          </label>
                         </div>
                       </div>
                     </div>
@@ -67,13 +44,13 @@
                       <div class="col-sm-8">
                         <div class="radio">
                           <label for="">
-                            <input type="radio" number v-model="giftcard.active" value="1">
+                            <input type="radio" number v-model="integralData.active" value="1" checked="checked">
                             是
                           </label>
                         </div>
                         <div class="radio">
                           <label for="">
-                            <input type="radio" number v-model="giftcard.active" value="0" checked="checked">
+                            <input type="radio" number v-model="integralData.active" value="0">
                             否
                           </label>
                         </div>
@@ -99,6 +76,8 @@
   </div>
 </template>
 <script>
+  /* eslint-disable semi */
+
   import Gallery from '../../../gallery/index.vue'
   import Editor from '../../../editor.vue'
   import api from 'api/index.js'
@@ -107,50 +86,8 @@
     components: [Gallery, Editor],
     data: function () {
       return {
-        limitTimeSales: false,
-        product: {
-          cat_id: null,
-          title: '',
-          sub_title: '',
-          priority: 0,
-          digest: '',
-          cover_image: '',
-          detail: '',
-          type: 'entity',
-          image_ids: [],
-          group_ids: [],
-          with_invoice: 0,
-          with_care: 0,
-          tags: '',
-          open_time: '',
-          end_time: '',
-          skus: []
-        },
-        sku: {
-          name: '',
-          cover_image: '',
-          display_price: 0,
-          price: 0,
-          unit: '瓶',
-          express_fee: 0,
-          bar_code: '',
-          stock: 0,
-          income_price: 0,
-          settle_price: 0,
-          attr_value_ids: []
-        },
-        editor: {},
-        images: [],
-        categories: [],
-        groups: []
-      }
-    },
-    route: {
-      data () {
-        return {
-          categories: api.categories.getAll(),
-          groups: api.groups.getAll()
-        }
+        limittype: '1',
+        integralData: {active: '', sort_type: '1', title: ''}
       }
     },
     watch: {
@@ -165,29 +102,11 @@
       }
     },
     methods: {
-      setCover (index, image) {
-        this.$set('product.cover_image', image.url)
-        this.images.forEach(function (val) {
-          val.cover = false
-        })
-        var oldImage = this.images[index]
-        oldImage.cover = true
-        this.images.$set(index, oldImage)
-      },
-      removeImage (image) {
-        this.images.$remove(image)
-      },
       openGallery: function (cb) {
         this.$broadcast('openGallery', cb)
       },
-      getSku () {
-        this.sku.cover_image = this.product.cover_image
-        this.sku.name = this.product.title
-        this.product.skus.push(this.sku)
-      },
       submit () {
         var self = this
-        this.getSku()
         api.products.create(this.product).then(function (da) {
           window.alert('创建成功!')
           self.$route.router.go('/dashboard/products/list')
